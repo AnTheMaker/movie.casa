c2777885b623470fe845faaf3b13f99abaf611d3
An <18481195+AnTheMaker@users.noreply.github.com>
2021-12-29 10:54:03 +0000

    automatically copy the content of the /static/ folder into the output directory

--- build.php
+++ build.php
@@ -3,6 +3,9 @@ $start_time = microtime(true);
 
 include_once __DIR__.'/functions.php';
 
+// copy the content of the /static/ folder into the output directory
+copy_dir(__DIR__.'/static', __DIR__.'/dist');
+
 // do stuff
 
 $time_elapsed = microtime(true)-$start; // execution time in seconds
