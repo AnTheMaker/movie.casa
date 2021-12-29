752cdafdd78eec296dca7211bdfb9ae80ca6c844
An <18481195+AnTheMaker@users.noreply.github.com>
2021-12-29 18:07:05 +0000

    Add "watch now" button which leads to tmdb

--- static/js/script.js
+++ static/js/script.js
@@ -20,6 +20,7 @@ $(function(){
             html += '<h2>'+title+'</h2>';
             html += '<p>'+desc+'</p>';
             html += '<p><svg class="star" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg> '+movie.votes.avg+'/10</p>';
+            html += '<a href="https://www.themoviedb.org/movie/'+movie.id+'" target="_blank"><button class="watch">Watch Now</button></a>';
           html += '</div>';
         html += '</div>';
       }else{
